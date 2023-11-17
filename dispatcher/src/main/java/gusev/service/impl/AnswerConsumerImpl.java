0af6b59b567bdb1fdd285231f9bce9bb1ac18a10
Your Name <dimagyss@mail.ru>
2023-11-18 00:08:59 +0300

    My last changes with rabbitmq

---
+++ dispatcher/src/main/java/gusev/service/impl/AnswerConsumerImpl.java
@@ -0,0 +1,24 @@
+package gusev.service.impl;
+
+import gusev.controller.UpdateController;
+import gusev.service.AnswerConsumer;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+import static ru.gusev.model.RabbitQueue.ANSWER_MESSAGE;
+
+@Service
+public class AnswerConsumerImpl implements AnswerConsumer {
+    private final UpdateController updateController;
+
+    public AnswerConsumerImpl(UpdateController updateController) {
+        this.updateController = updateController;
+    }
+
+    @Override
+    @RabbitListener(queues = ANSWER_MESSAGE)
+    public void consume(SendMessage sendMessage) {
+        updateController.setView(sendMessage);
+    }
+}
